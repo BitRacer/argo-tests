09819aabe2a53c1e9749f02ae83c345fcd7867d0
Aristotle <aristotle.allen@gmail.com>
2022-11-19 15:07:24 -0600

    fixed containers and probes

--- containers/canary/build.sh
+++ containers/canary/build.sh
@@ -1,6 +1,7 @@
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/canary:1.0 .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/canary:1.1 .
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/canary:latest .
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/bgc:green .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/bgc:canary .
 
 
 
+cd ..
--- containers/green/build.sh
+++ containers/green/build.sh
@@ -1,4 +1,4 @@
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/green:1.0 .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/green:1.1  .
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/green:latest .
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/bgc:green .
 
