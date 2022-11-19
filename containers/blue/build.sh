95622ca52470957e4666582955daa90596e6a7dc
Aristotle <aristotle.allen@gmail.com>
2022-11-19 11:23:04 -0600

    upadting blue container

--- containers/blue/build.sh
+++ containers/blue/build.sh
@@ -1,4 +1,4 @@
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/blue:1.0 .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/blue:1.1 .
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/blue:latest .
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/bgc:blue .
 
