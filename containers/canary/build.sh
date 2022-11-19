029372d1a50442d8397fd311cf4c7495821f10bb
Aristotle <aristotle.allen@gmail.com>
2022-11-19 12:28:38 -0600

    fixing green

--- containers/canary/build.sh
+++ containers/canary/build.sh
@@ -1,5 +1,5 @@
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/green:1.0 .
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/green:latest .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/canary:1.0 .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/canary:latest .
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/bgc:green .
 
 
