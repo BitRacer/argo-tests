06afa3ce97af0b43330c9f57573597b3312cea06
Aristotle <aristotle.allen@gmail.com>
2022-11-19 10:37:17 -0600

    adding dockerfiles and updated blue to deployment

---
+++ containers/blue/build.sh
@@ -0,0 +1,6 @@
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/blue:1.0 .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/blue:latest .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/bgc:blue .
+
+
+
---
+++ containers/green/build.sh
@@ -0,0 +1,6 @@
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/green:1.0 .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/green:latest .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag aballen/bgc:green .
+
+
+
